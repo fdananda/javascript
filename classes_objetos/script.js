8cc79944686f14829881923ad74ae20b24135e74
Fernanda Moreira (fdananda) <67290766+fdananda@users.noreply.github.com>
2023-03-16 19:29:40 -0700

    classes_objetos

---
+++ classes_objetos/script.js
@@ -0,0 +1,19 @@
+class Pessoa {
+  constructor(nome, nascimento) {
+    this.nome       = nome;
+    this.nascimento = nascimento;
+  }
+
+  calcularIdade(){
+    const dataAtual = new Date();
+    return dataAtual.getFullYear() - this.nascimento;
+  }
+}
+
+function gerarObjetoPessoa() {   
+
+    const objetoPessoa = new Pessoa("Fernanda", 1977)
+
+    let saida = document.getElementById("exemplos")
+    saida.innerHTML = `A pessoa é ${objetoPessoa.nome}, nascida em ${objetoPessoa.nascimento} e com ${objetoPessoa.calcularIdade()} anos.`
+}
