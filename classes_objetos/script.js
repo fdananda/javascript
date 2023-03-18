f660a03aa267b1c106dee61ee186f942f2f8cacd
Fernanda Moreira (fdananda) <67290766+fdananda@users.noreply.github.com>
2023-03-18 05:44:12 -0700

    classes_objetos

--- classes_objetos/script.js
+++ classes_objetos/script.js
@@ -1,3 +1,4 @@
+//Opção 1 - Exemplo de criação de classe e objeto
 class Pessoa {
   constructor(nome, nascimento) {
     this.nome       = nome;
@@ -17,3 +18,39 @@ function gerarObjetoPessoa() {
     let saida = document.getElementById("exemplos")
     saida.innerHTML = `A pessoa é ${objetoPessoa.nome}, nascida em ${objetoPessoa.nascimento} e com ${objetoPessoa.calcularIdade()} anos.`
 }
+
+/*Opção 2 - Exemplo de Notação Literal de objeto
+const objetoPessoa = {
+
+   nome       : "Fernanda",
+   nascimento : 1977,
+
+   calcularIdade: function(){
+      const dataAtual = new Date();
+      return dataAtual.getFullYear() - this.nascimento;
+    }
+  }
+
+function gerarObjetoPessoa() {  
+
+  let saida = document.getElementById("exemplos")
+  saida.innerHTML = `A pessoa é ${objetoPessoa.nome}, nascida em ${objetoPessoa.nascimento} e com ${objetoPessoa.calcularIdade()} anos.`
+}
+*/
+
+/*Opção 3 - Exemplo de Notação de Construtor (objeto em branco)
+const objetoPessoa = new Object()
+
+objetoPessoa.nome       = "Fernanda"
+objetoPessoa.nascimento = 1977
+objetoPessoa.calcularIdade = function(){
+      const dataAtual = new Date();
+      return dataAtual.getFullYear() - this.nascimento;    
+}
+
+function gerarObjetoPessoa() {  
+
+  let saida = document.getElementById("exemplos")
+  saida.innerHTML = `A pessoa é ${objetoPessoa.nome}, nascida em ${objetoPessoa.nascimento} e com ${objetoPessoa.calcularIdade()} anos.`
+}
+*/
