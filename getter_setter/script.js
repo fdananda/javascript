7b698221641904a8110832deb9123f0fb247ba63
Fernanda Moreira (fdananda) <67290766+fdananda@users.noreply.github.com>
2023-03-20 20:06:23 -0700

    getter_setter

---
+++ getter_setter/script.js
@@ -0,0 +1,38 @@
+//Opção 1 - Exemplo de criação de classe e objeto
+class Pessoa {
+  constructor() {
+    this._nome       = "";
+    this._nascimento = 0;
+  }
+
+  get nome(){
+    return this._nome
+  }
+
+  set nome(nome){
+    this._nome = nome
+  }
+
+  get nascimento(){
+    return this._nascimento
+  }
+
+  set nascimento(nascimento){
+    this._nascimento = nascimento
+  }
+
+  calcularIdade(){
+    const dataAtual = new Date();
+    return dataAtual.getFullYear() - this.nascimento;
+  }
+}
+
+function gerarObjetoPessoa() {   
+
+    const objetoPessoa = new Pessoa()
+    objetoPessoa.nome = "Fernanda"
+    objetoPessoa.nascimento = 1977
+
+    let saida = document.getElementById("exemplos")
+    saida.innerHTML = `A pessoa é ${objetoPessoa.nome}, nascida em ${objetoPessoa.nascimento} e com ${objetoPessoa.calcularIdade()} anos.`
+}
