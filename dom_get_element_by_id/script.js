ae3728a6739554ea3821d3bb411aefa327f32430
Fernanda Moreira (fdananda) <67290766+fdananda@users.noreply.github.com>
2023-03-31 11:10:21 -0700

    dom_get_element_by_id

--- dom_get_element_by_id/script.js
+++ dom_get_element_by_id/script.js
@@ -1,4 +1,4 @@
-function obterElemento(id) {
+function obterElementoPeloId(id) {
 
 	let get_id = document.getElementById(id)
 
@@ -6,9 +6,13 @@ function obterElemento(id) {
 
 	let textoSaida = 
 	`O número recuperado é: ${numeroConvertido}<br>
+	typeof numeroConvertido: ${typeof numeroConvertido}<br>
 	get_id: ${get_id}<br>
+	typeof get_id: ${typeof get_id}<br>
 	get_id.value: ${get_id.value}<br>
+	typeof get_id.value: ${typeof get_id.value}<br>
 	get_id.id: ${get_id.id}<br>
+	typeof get_id.id: ${typeof get_id.id}<br>
 	`
 
 	let saida = document.getElementById("exemplos")
