9c03b87957d69af9b32a84c04f4c9041094a0a15
Fernanda Moreira (fdananda) <67290766+fdananda@users.noreply.github.com>
2023-04-06 19:11:51 -0700

    api_fetch

---
+++ api_fetch/script.js
@@ -0,0 +1,18 @@
+function obterDadosWeb(url){
+
+  fetch(url).then(response => {
+      if(!response.ok){
+        alert("Falha no processamento!")
+        return
+      }
+      response.json().then(resposta => {
+        let saida = document.getElementById("exemplos")
+        saida.innerHTML = `
+        JSON convertido a partir da internet - id: ${resposta.id}<br>
+        JSON convertido a partir da internet - date: ${resposta.date}<br>
+        JSON convertido a partir da internet - title: ${resposta.title.rendered}<br>
+        JSON convertido a partir da internet - link: ${resposta.link}<br>
+        `
+      }).catch(erro => alert("Erro de JSON: " + erro))
+    }).catch(erro => alert("Erro na busca: " + erro))
+}
